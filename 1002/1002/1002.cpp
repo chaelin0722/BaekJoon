4ea80e4b7bd4d1e1b9877fab98e3399e02557b27
alicia <53431568+chaelin0722@users.noreply.github.com>
2020-05-22 21:43:18 +0900

    1002추가

---
+++ 1002/1002/1002.cpp
@@ -0,0 +1,27 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+//#pragma warning(disable: 4996)
+#include<stdio.h>
+#include<stdlib.h>
+#include <math.h>
+
+int main(void) {
+	int x1, y1, r1, x2, y2, r2, i, time;
+	double d = 0;
+
+	scanf("%d", &time);
+
+	for (i = 0; i < time; i++) {
+		scanf("%d%d%d%d%d%d", &x1, &y1, &r1, &x2, &y2, &r2);
+
+		d = sqrt(pow((x1 - x2), 2) + pow((y1 - y2), 2));
+
+		if (r1 + r2 < d) printf("0\n");
+		else if (abs(r1 - r2) > d) printf("0\n");
+		else if (d == 0 && r1 == r2) printf("-1\n");
+		else if (d == (r1 + r2)) printf("1\n");
+		else if (d == abs(r1 - r2)) printf("1\n");
+		else printf("2\n");
+	}//end for
+	
+	return 0;
+}
