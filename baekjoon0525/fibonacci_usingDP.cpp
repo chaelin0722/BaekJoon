4efda80127ca4dca439ca883fa17cee5a2e95a35
alicia <53431568+chaelin0722@users.noreply.github.com>
2020-05-26 23:04:50 +0900

    재귀함수 공부

--- baekjoon0525/fibonacci_usingDP.cpp
+++ baekjoon0525/fibonacci_usingDP.cpp
@@ -1,8 +1,4 @@
-//#include <algorithm>
-#include <iostream>
-using namespace std;
 
-/*
 int d[41][2] = { {1,0},{0,1} };
 int main()
 {
@@ -20,36 +16,4 @@ int main()
 	return 0;
 }
 
-*/
-int dp[41];
-
-int fibonacci(int n){
-	if(n <= 0) return 0;
-	else if(n == 1) return 1;
-    
-    if(dp[n] != 0) return dp[n];
-    else{
-    	return dp[n] = fibonacci(n-1) + fibonacci(n-2);
-	}
-}
-
-int main(){
-	int n;
-	cin >>n;
-	while(n-- > 0){
-		int x;
-		cin>> x;
-		
-		if(x==0){
-			cout<<"1 0"<<"\n";
-		}else if(x==1){
-			cout<< "0 1"<<"\n";
-		}else{
-			fibonacci(x);
-			cout<<dp[x-1]<<" "<<dp[x]<<"\n";
-		}
-	}
-	
-	return 0;
-}
 
