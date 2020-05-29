38ad500e5bb3886a6e2a11592c9efa7bde1b067a
alicia <53431568+chaelin0722@users.noreply.github.com>
2020-05-29 23:12:15 +0900

    시간초과 문제 발생 복잡도nlogn 으로 사용해야함 => quick이나 merge

--- baekjoon0529/2751.cpp
+++ baekjoon0529/2751.cpp
@@ -1,3 +1,9 @@
+/*제한조건! 
+시간 : 1초
+메모리 : 128MB
+따라서 아래코드는 시간 초과 발생.. mergesort 나 quick sort 사용 필요 
+*/
+
 #include<iostream>
 #include<algorithm>
 using namespace std;
@@ -17,8 +23,5 @@ int main(void){
 	for(i=0;i<n;i++){
 		cout<<arr[i]<<endl;
 	}
-	
-	
-	
 	return 0;
 }
