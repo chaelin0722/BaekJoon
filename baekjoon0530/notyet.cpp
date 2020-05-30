8f649ce01a60b0d9e70f2979556d03bbc1ba815b
alicia <53431568+chaelin0722@users.noreply.github.com>
2020-05-31 00:49:25 +0900

    지금 이 방식 시간초과아아

--- baekjoon0530/notyet.cpp
+++ baekjoon0530/notyet.cpp
@@ -3,31 +3,26 @@
 #include<vector>
 using namespace std;
 
-vector<int> x;
-vector<int> y;
 
 int main(){
-	int n, i,j,x_value,y_value;
+	
+	int n, i;
 	cin>>n;
-int y1;
+	
+	vector< vector<int> > arr(n, vector<int>(2,0)); //n*2로 초기화 
+	// n*m 사이즈인 int형 벡터 arr을 0으로 초기화 
+	/*
+	   vector<vector<int>> arr;
+	   arr.assign(n, vector<int>(m,0)); 과 같다  
+	*/ 
+	
+	
 	for(i=0;i<n;i++){		
-		cin>>x_value>>y_value;
-		x.push_back(x_value);
-		y.push_back(y_value);
+		cin>>arr[i][0]>>arr[i][1]; 
 	}
-	sort(x.begin(), x.end());
-	for(i=0;i<n-1;i++){
-		for(j=i+1;j<n;j++){
-			if(x[i]==x[j]) {
-				y = y+i-1;
-				y1 = y+i;
-			}sort(y, y1);
-		}		
-		cout<<x[i]<<"\t"<<y[i]<<endl;
-		
-		
+	sort(arr.begin(), arr.end());
+	for(i=0;i<n;i++){
+		cout<<arr[i][0]<<" "<<arr[i][1]<<endl;
 	}
-	
-	
 	return 0;
 }
