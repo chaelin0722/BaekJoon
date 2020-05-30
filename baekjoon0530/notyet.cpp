8ab875cfae252db507034e9c5c45730fab8e92c0
alicia <53431568+chaelin0722@users.noreply.github.com>
2020-05-31 00:22:45 +0900

    x좌표와 y좌표를 따로 해서 먼저 x좌표를 정렬하고 같다면 y좌표를 정렬하려 했는데 더 간단한 방법인 2차원벡터를 사용하는 방법이 있다!

--- baekjoon0530/notyet.cpp
+++ baekjoon0530/notyet.cpp
@@ -1,8 +1,33 @@
 #include<iostream>
-
+#include<algorithm>
+#include<vector>
 using namespace std;
 
+vector<int> x;
+vector<int> y;
+
 int main(){
+	int n, i,j,x_value,y_value;
+	cin>>n;
+int y1;
+	for(i=0;i<n;i++){		
+		cin>>x_value>>y_value;
+		x.push_back(x_value);
+		y.push_back(y_value);
+	}
+	sort(x.begin(), x.end());
+	for(i=0;i<n-1;i++){
+		for(j=i+1;j<n;j++){
+			if(x[i]==x[j]) {
+				y = y+i-1;
+				y1 = y+i;
+			}sort(y, y1);
+		}		
+		cout<<x[i]<<"\t"<<y[i]<<endl;
+		
+		
+	}
+	
 	
 	return 0;
 }
