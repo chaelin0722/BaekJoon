87ce7460893611995926eba1d0ecd500c5b435bc
alicia <53431568+chaelin0722@users.noreply.github.com>
2020-06-02 20:40:15 +0900

    18014다시하기

---
+++ baekjoon0602/18014.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+using namespace std;
+vector<string> name;
+int main(void){
+	int n;
+	cin>>n;
+	
+	int i;
+	for(i=0;i<n;i++){
+		
+		name.push_back(n)
+		
+	}
+	
+	
+	
+	return 0;
+}
