1b0ce01be3cbfd228ed7644517aaca6c082b3a21
alicia <53431568+chaelin0722@users.noreply.github.com>
2020-06-03 21:18:23 +0900

    backtracking을 이용한 15649

---
+++ baekjoon0603/15649.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+#define MAX 9
+
+int N,M;
+int lists[MAX];
+bool check[MAX] = {0, };
+
+void dfs(int cnt){
+	if(cnt == M){
+		for(int i=0;i<M; i++){
+			cout<<lists[i]<<" ";
+		}
+		cout<<endl;
+		return;
+	}
+
+	for(int i=1; i<=N; i++){
+		if(!check[i]){
+			check[i] = true;
+			lists[cnt] = i;
+			dfs(cnt+1);
+			check[i] = false;
+		}
+	}	
+}
+int main(void){
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	
+	cin>>N>>M;
+	dfs(0);	
+	return 0;
+}
