deb0b970711b9f9ff24e7b67e8d2505e68c3f643
alicia <53431568+chaelin0722@users.noreply.github.com>
2020-06-06 23:44:58 +0900

    별찍기 예제.. 은근 머리 아프단 말이지.. 기본인데.. 연습연습

--- baekjoon0606/10950.cpp
+++ baekjoon0606/10950.cpp
@@ -1,15 +1,45 @@
+/*
 #include<iostream>
 using namespace std;
 
 int main(void){
-	int n,a,b;
+	int n;
 	cin>>n;
-
-	for(int i=0;i<n;i++){
-		cin>>a>>b;
-		cout << a+b<<endl;
-	}
 	
+	for(int i=0;i<n;i++){
+		for(int k=1;k<n-1;k++) cout<<" ";
+		for(int j=0;j<i+1;j++) cout<<"*";
+		
+		cout<<endl;
+	}
+
+
+
 	return 0;
 }
 
+*/
+
+#include <iostream>
+#include <algorithm>
+using namespace std;
+ 
+int main(int argc, char* argv[])
+{
+	int n;
+ 
+	cin >> n;
+ 
+	for (int i = 1; i <= n; ++i) {
+		for (int j = 1; j <= n - i; ++j) {
+			cout << " ";
+		}
+		
+		for (int j = 1; j < i; ++j) {
+			cout << "* ";
+		}
+		cout << "*\n";
+	}
+ 
+	return 0;
+}
