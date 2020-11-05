9bb6dbb90c12f1a494f230b5dfc6a779ba06dc89
김채린 <ccaa9697@gmail.com>
2020-11-05 11:15:45 +0900

    arrange

--- Oct/thread.c
+++ Oct/thread.c
@@ -17,6 +17,8 @@ thread_function(void *arg)
     fflush(stdout);  /*출력 버퍼 비우는 용도*/
     sleep(1);
     myglobal=j;
+    
+    printf("tread +?   %d\n", myglobal);
   }
   return NULL;
 }
@@ -27,16 +29,7 @@ main(void)
   pthread_t mythread;
   int i;
 
-  if ( pthread_create( &mythread, NULL, thread_function, NULL) ) {  /*첫번재 매개변수는 쓰레드가 성공적으로
-  												생성 되었을 때 생성된 쓰레드를 식별하기 위해 사용되는 쓰레드 식별자
-												  두번째 매개변수는 쓰레드 특성 지정을 위해 사용, 기본 쓰레드 특성을 
-												사용하고 싶으면 NULL.
-												세번째 매게변수는 분기시켜서 실행할 쓰레드 함수.
-												네번째 매개변수는 세번째 매개변수인 쓰레드 함수의 매개변수로 넘겨진다.
-		 int pthread_create(pthread_t *thread, const pthread_attr_t *attr, void *(*start_routine)(void *), void *arg);
-
-출처: https://bitsoul.tistory.com/157 [Happy Programmer~]
-												  */
+  if ( pthread_create( &mythread, NULL, thread_function, NULL) ) {
     printf("error creating thread.");
     abort();
   }
@@ -44,6 +37,8 @@ main(void)
   for ( i=0; i<20; i++) {
     myglobal=myglobal+1;
     printf("o");
+    printf("\n");
+    printf("myglobal = %d\n", myglobal);
     fflush(stdout);
     sleep(1);
   }
@@ -57,3 +52,15 @@ main(void)
 
   exit(0);
 }
+
+
+  /*첫번재 매개변수는 쓰레드가 성공적으로
+  												생성 되었을 때 생성된 쓰레드를 식별하기 위해 사용되는 쓰레드 식별자
+												  두번째 매개변수는 쓰레드 특성 지정을 위해 사용, 기본 쓰레드 특성을 
+												사용하고 싶으면 NULL.
+												세번째 매게변수는 분기시켜서 실행할 쓰레드 함수.
+												네번째 매개변수는 세번째 매개변수인 쓰레드 함수의 매개변수로 넘겨진다.
+		 int pthread_create(pthread_t *thread, const pthread_attr_t *attr, void *(*start_routine)(void *), void *arg);
+
+출처: https://bitsoul.tistory.com/157 [Happy Programmer~]
+												  */
